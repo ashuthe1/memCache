b07543a7e76b016fd4dd31c9ce42a8c3b7e01d4c
ashuthe1 <ashutosh251848@gmail.com>
2024-07-08 16:19:23 +0530

    Tested BatchSet() and BatchGet() functionality

--- sample/main.go
+++ sample/main.go
@@ -8,65 +8,21 @@ import (
 	"github.com/ashuthe1/kuki-memcache/eviction"
 )
 
-type CustomEviction struct {
-	// Define your custom data structure and fields here
-}
-
-func NewCustom() eviction.EvictionPolicy {
-	return &CustomEviction{
-		// Initialize your custom data structure here
-	}
-}
-
-func (c *CustomEviction) Add(key string) {
-	// Implement your custom eviction policy logic for Add operation
-}
-
-func (c *CustomEviction) Remove(key string) {
-	// Implement your custom eviction policy logic for Remove operation
-}
-
-func (c *CustomEviction) Evict() string {
-	// Implement your custom eviction policy logic for Evict operation
-	return "" // Adjust the return value as per your implementation
-}
-
-type Student struct {
-	Name string
-	age  int
-}
-
 func main() {
-	// Example: Create a cache with 2 seconds TTL, size of 2 items, and custom eviction policy
-	c := cache.NewCache(2*time.Second, 2, NewCustom(), nil)
+	c := cache.NewCache(20*time.Minute, 10, eviction.NewLRU(), nil)
 
-	totalRegisteredUsers := 100
-	c.Set("totalRegisteredUsers", totalRegisteredUsers)
-
-	value, found := c.Get("totalRegisteredUsers")
-	if found {
-		fmt.Println(value)
+	// Batch set key-value pairs
+	items := map[string]interface{}{
+		"key1": "value1",
+		"key2": 123,
+		"key3": []int{1, 2, 3},
 	}
+	c.BatchSet(items)
 
-	student1 := Student{Name: "Ashutosh", age: 22}
-	arr := []int{1, 2, 3, 4, 5}
-	c.Set("studentId:1", student1)
-
-	c.Set("arr", arr)
-
-	// Get the value from the cache
-	value, found = c.Get("studentId:1")
-	if found {
-		student := value.(Student)
-		fmt.Printf("%+v\n", student)
-	}
-
-	value, found = c.Get("arr")
-	if found {
-		fetchedArr := value.([]int)
-		for _, v := range fetchedArr {
-			print(v, " ")
-		}
-		println()
+	// Batch get values
+	keys := []string{"key1", "key2", "key3"}
+	values := c.BatchGet(keys)
+	for k, v := range values {
+		fmt.Printf("%s: %v\n", k, v)
 	}
 }
